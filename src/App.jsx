50a462427323a0440bcbc0abf78b935ed66c4f3a
hlalgvnc <hilalguvenc@anadolu.edu.tr>
2020-02-16 12:36:15 +0300

    break and session components added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "./App.css";
+import Break from "./components/Break";
+import Session from "./components/Session";
 
 function App() {
   return (
---
+++ src/components/Break.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+function Break() {
+  return <div></div>;
+}
+export default Break;
