3718a518b6342dd35ce9b7f107c59a40fb90b2fc
hlalgvnc <hilalguvenc@anadolu.edu.tr>
2020-02-19 16:52:39 +0300

    updated

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,67 +1,45 @@
-import React, { useState } from "react";
-import Break from "./Break";
+import React, { useState, useEffect } from "react";
 
 function Timer(props) {
   const [isSession, setIsSession] = useState(true);
   const [timerSecond, setTimerSecond] = useState(0);
-  const [intervalId, setIntervalId] = useState(0);
 
-  const decreaseTimer = () => {
-    switch (timerSecond) {
-      case 0:
-        if (props.timerMinute === 0) {
-          if (isSession) {
-            setIsSession({ isSession: false });
-            props.toggleInterval(isSession);
-          } else {
-            setIsSession({ isSession: true });
-            props.toggleInterval(isSession);
-          }
-        }
-        setTimerSecond(59);
-        props.updateTimerMinute();
-        break;
-      default:
-        setTimerSecond(timerSecond - 1);
-    }
-  };
-    
-  const start = () => {
-    let intervalId = setInterval(decreaseTimer, 1000);
-    setIntervalId( intervalId  );
-  };
+  useEffect(() => {
+    let intervalId = setInterval(() => {
+      switch (timerSecond) {
+        case 0:
+          setTimerSecond(59);
+          props.updateTimerMinute();
+          break;
+        default:
+          setTimerSecond(timerSecond - 1);
+      }
+    }, 1000);
+    return () => clearInterval(intervalId);
+  }, [timerSecond]);
 
-  const reset = () => {
-    clearInterval(intervalId);
-    props.reset();
-    setTimerSecond(timerSecond);
-  };
   return (
     <div>
       <h4 className="session">{isSession === true ? "Session" : "Break"}</h4>
       <div className="timer">
-      <span>{props.timerMinute}</span>
-      <span>:</span>
-      <span>
-        {timerSecond === 0
-          ? "00"
-          : timerSecond< 10
-          ? "0" + timerSecond
-          : timerSecond}
-      </span>
+        <span>{props.timerMinute}</span>
+        <span>:</span>
+        <span>
+          {timerSecond === 0
+            ? "00"
+            : timerSecond < 10
+            ? "0" + timerSecond
+            : timerSecond}
+        </span>
       </div>
       <div className="wrapper">
-        <button onClick={start} className="start">
-          Start
-        </button>
+        <button className="start">Start</button>
         <img
           className="start-icon"
           src={`https://image.flaticon.com/icons/svg/254/254434.svg`}
           alt=""
         />
-        <button onClick={reset} className="reset">
-          Reset
-        </button>
+        <button className="reset">Reset</button>
         <img
           className="reset-icon"
           src={`https://img.icons8.com/ios-filled/2x/recurring-appointment.png`}
